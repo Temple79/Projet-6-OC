8cbddc8b474e6164ce8dcce14e76afe5a10157ce
Temple79 <126321231+Temple79@users.noreply.github.com>
2023-11-21 11:07:50 +0200

    Création des routes

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+
+function Banner(props) {
+    return (
+        <div className={props.class}>
+            img src={props.image} alt={props.alt} />{props.children}
+        </div>
+    )
+}
+
+export default Banner
